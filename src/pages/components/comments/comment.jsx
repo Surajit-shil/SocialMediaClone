68185ea7568a0d17437d1eeed967175954353096
Surajit <shilsurajit12@gmail.com>
2023-06-23 09:52:07 +0530

    Added facility of new-comments

--- src/pages/components/comments/comment.jsx
+++ src/pages/components/comments/comment.jsx
@@ -1,14 +1,16 @@
-import { useContext } from 'react';
+import { useContext,useState } from 'react';
 import './comment.scss'
 import { AuthContext } from '../../../Context/Auth';
 function Comment(){
     const {currUser}=useContext(AuthContext)
+    const [newComment,setnewComment]=useState([]);
     const comments = [
         {
           id: 1,
           desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
           name: "John Doe",
           userId: 1,
+          duration:'1 hour ago',
           profilePicture:
             "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         },
@@ -17,26 +19,42 @@ function Comment(){
           desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
           name: "Jane Doe",
           userId: 2,
+          duration:'10 min ago',
           profilePicture:
             "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
         },
       ];
+      const [totalComment,setTotalComments]=useState(comments)
+      const changeHandle=(e)=>{
+         setnewComment(e.target.value);
+      }
+      const clickHandle=()=>{
+          document.getElementById('input-text').value=''
+          setTotalComments([...comments,{
+            id: totalComment.length+comments.length+1,
+            desc:newComment,
+            name: "Surajit Shil",
+            userId: totalComment.length+comments.length+1,
+            duration:'few secs ago',
+            profilePicture:currUser.profilePic 
+          }])
+      }
     return(
        <div className="comments">
         <div className="write">
           <img src={currUser.profilePic}></img>
-          <input type='text' placeholder='write a comment'></input>
-           <button>Send</button>
+          <input type='text' placeholder='write a comment' onChange={changeHandle} id='input-text'></input>
+           <button onClick={clickHandle}>Send</button>
         </div>
         {
-            comments.map(comment=>(
-              <div className="comment">
+            totalComment.map(comment=>(
+              <div className="comment" key={comment.id}>
                 <img src={comment.profilePicture}></img>
                 <div className="info">
                     <span>{comment.name}</span>
                     <p>{comment.desc}</p>
                 </div>
-                <span className='date'>1 hour ago</span>
+                <span className='date'>{comment.duration}</span>
               </div>
             ))
         }
